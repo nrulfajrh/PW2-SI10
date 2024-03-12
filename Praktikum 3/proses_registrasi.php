88d8083370894d0884757cf64bd367deede98608
nrulfajrh <nuru23211si@student.nurulfikri.ac.id>
2024-03-12 15:04:30 +0700

    Praktikum 3

--- Praktikum 3/proses_registrasi.php
+++ Praktikum 3/proses_registrasi.php
@@ -8,7 +8,40 @@ $skills = $_POST['skill'];
 $domisili = $_POST['domisili'];
 $email = $_POST['email'];
 
+$nilai_skill = [
+    'HTML' => 10,
+    'CSS' => 10,
+    'JavaScript' => 20,
+    'RWD Bootstrap' => 20,
+    'PHP' => 30,
+    'Pyhon' => 30,
+    'Java' => 50
+];
 
+
+//menghitung skor skills
+$total_skor = 0;
+foreach ($skills as $value){
+    $total_skor += $nilai_skill[$value];
+}
+
+//mencari kategori skill berdasarkan skor
+$kategori_skill = '';
+if ($total_skor == 0) {
+    $kategori_skill = 'Tidak memadai';
+}
+else if ($total_skor <= 40){
+    $kategori_skill = 'Kurang';
+}
+else if ($total_skor <= 60){
+    $kategori_skill = 'Cukup';
+}
+else if ($total_skor <= 100){
+    $kategori_skill = 'Baik';
+}
+else if ($total_skor <= 170){
+    $kategori_skill = 'Sangat Baik';
+}
 
 
 echo "NIM: $nim";
@@ -18,3 +51,6 @@ echo "<br> Program Studi: $prodi";
 echo "<br> Skill Programming: " . join(',', $skills);
 echo "<br> Domisili: $domisili";
 echo "<br> email: $email";
+echo "<br> Skor Skill: $total_skor";
+echo "<br> Kategori Skill: $kategori_skill";
+
