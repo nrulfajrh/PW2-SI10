e45cb074d112ab77e7b0d7f622c9888773d7a56b
nrulfajrh <146635642+nrulfajrh@users.noreply.github.com>
2024-03-27 21:35:26 +0700

    Delete tugas praktikum 5 directory

--- tugas praktikum 5/persegipanjang/data_persegipanjang.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('class_persegipanjang.php');
-
-$persegiPanjang = new PersegiPanjang(5, 3);
-
-echo "Luas Persegi Panjang: " . $persegiPanjang->hitungLuas() . "<br>";
-echo "Keliling Persegi Panjang: " . $persegiPanjang->hitungKeliling();
